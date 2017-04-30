8cae6773c7b57778805835bbf29727171f938560
PANCERZ <tpancerz2@gmail.com>
2017-04-30 10:40:33 +0200

    change project tree

--- GAME/GAME.pro
+++ GAME/GAME.pro
@@ -1,8 +1,12 @@
 TEMPLATE = app
-CONFIG += console c++11
+CONFIG += console c++1z
 CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
 INCLUDEPATH += /usr/include/SFML
 LIBS += -L/usr/lib/x86_64-linux-gnu/ -lsfml-graphics -lsfml-window -lsfml-system
+
+
+INCLUDEPATH += /usr/include/boost
+LIBS += -L/usr/lib/ -lboost_filesystem -lboost_system
--- GAME/main.cpp
+++ GAME/main.cpp
@@ -1,11 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include<boost/filesystem.hpp>
+
+#include<iostream>
+
+
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
-    //sf::Texture texture;
-    //texture.loadFromFile(("/home/pancerz/Desktop/GAME/GAME/images/space.jpg"));
-    //sf::Sprite sprite(texture);
+    sf::Texture texture;
+    texture.loadFromFile("space.jpg");
+    sf::Sprite sprite(texture);
+    std::cout<<boost::filesystem::current_path();
 
     while (window.isOpen())
     {
@@ -17,7 +23,7 @@ int main()
         }
 
         window.clear();
-        //window.draw(sprite);
+        window.draw(sprite);
         window.display();
     }
 
