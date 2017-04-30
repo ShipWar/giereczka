9c38b2a9ff46f4d450ba59cc630d9a080529920a
PANCERZ <tpancerz2@gmail.com>
2017-04-30 13:07:32 +0200

    remove filesystem

--- GAME/main.cpp
+++ GAME/main.cpp
@@ -1,17 +1,13 @@
 #include <SFML/Graphics.hpp>
-#include<boost/filesystem.hpp>
-
-#include<iostream>
-
 
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+
     sf::Texture texture;
     texture.loadFromFile("space.jpg");
     sf::Sprite sprite(texture);
-    std::cout<<boost::filesystem::current_path();
 
     while (window.isOpen())
     {
