bddfd0882538399b31eadb05c3b5c11a29bfd9e2
Pokrywka <robertpo93@o2.pl>
2017-05-01 15:28:26 +0200

    add ship control

--- game.cpp
+++ game.cpp
@@ -2,7 +2,7 @@
 
 void Game::createTextures()
 {
-    m_window.create(sf::VideoMode(800, 400), "SFML works!");
+    m_window.create(sf::VideoMode(800, 700), "SFML works!");
 
     m_spaceTexture.loadFromFile("space.jpg");
 
@@ -22,13 +22,32 @@ void Game::startGame()
 
 void Game::mainLoop()
 {
+
     while (m_window.isOpen())
     {
         sf::Event event;
         while (m_window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)))
                 m_window.close();
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                m_shipSprite.move(20,0);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                m_shipSprite.move(-20,0);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                m_shipSprite.move(0,-20);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                m_shipSprite.move(0,20);
+            }
+
         }
 
         m_window.clear();
