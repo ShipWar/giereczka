b05faa6f9463af0dd7a87ac8f16225cd8e656093
Pokrywka <robertpo93@o2.pl>
2017-05-01 18:04:08 +0200

    initial position of the ship

--- game.cpp
+++ game.cpp
@@ -1,15 +1,20 @@
 #include "game.h"
 
+constexpr int windowSizeX = 800;                                    //Szerokość okna
+constexpr int windowSizeY = 700;                                    //Wysokość okna
+
+sf::Vector2u shipSize;                                              //Stworzenie wektora przechowującego wymiary statku
+
 void Game::createTextures()
 {
-    m_window.create(sf::VideoMode(800, 700), "SFML works!");
+    m_window.create(sf::VideoMode(windowSizeX,windowSizeY), "SFML works!");
 
     m_spaceTexture.loadFromFile("space.jpg");
 
     m_spaceSprite.setTexture(m_spaceTexture);
 
     m_shipTexture.loadFromFile("DurrrSpaceShip.png");
-
+shipSize = m_shipTexture.getSize();                                 //Pobranie wymiaru statku
     m_shipSprite.setTexture(m_shipTexture);
 
 }
@@ -22,7 +27,7 @@ void Game::startGame()
 
 void Game::mainLoop()
 {
-
+    m_shipSprite.setPosition((windowSizeX-shipSize.x)/2,windowSizeY-shipSize.y);         //Wyśrodkowanie statku na dole okna
     while (m_window.isOpen())
     {
         sf::Event event;
@@ -54,5 +59,7 @@ void Game::mainLoop()
         m_window.draw(m_spaceSprite);
         m_window.draw(m_shipSprite);
         m_window.display();
+
     }
+
 }
