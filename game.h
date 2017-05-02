4a17024ced12f6e17ed4ad3f3d9b84785ae332f0
Pokrywka <robertpo93@o2.pl>
2017-05-02 04:13:11 +0200

    ship movement restriction - file game.h update

--- game.h
+++ game.h
@@ -1,6 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
 #include<SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
 class Game
 {
@@ -8,13 +10,14 @@ public:
     void createTextures();
     void startGame();
     void mainLoop();
+    static const int windowWidth = 800;
+    static const int windowHeight = 700;
 private:
     sf::RenderWindow m_window;
     sf::Texture m_spaceTexture;
     sf::Sprite m_spaceSprite;
     sf::Texture m_shipTexture;
     sf::Sprite m_shipSprite;
-
 };
 
 #endif // GAME_H
