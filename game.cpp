6483842bff7fd4b87255ccd00977be0ad95340e2
PANCERZ <tpancerz2@gmail.com>
2017-05-04 21:24:02 +0200

    fix for cleanup

--- game.cpp
+++ game.cpp
@@ -25,17 +25,10 @@ void Game::mainLoop()
     GameDisplay l_gameDisplay(m_window, m_spaceSprite, m_shipSprite);
     l_gameDisplay.setInitialPositionForObjects();
 
-<<<<<<< HEAD
     GameLogic l_gameLogic(m_window, m_spaceSprite, m_shipSprite);
-=======
-l_gameLogic.InitialPosition(m_window, m_shipTexture);
-m_shipSprite.setPosition(l_gameLogic.shipPosition);
->>>>>>> 42ad3b5a7f3477d3e7179ce446a59c3c2d8df176
 
     while (m_window.isOpen())
     {
-
-<<<<<<< HEAD
         while (m_window.pollEvent(m_event))
         {
             closeWindow();
@@ -44,15 +37,6 @@ m_shipSprite.setPosition(l_gameLogic.shipPosition);
         }
 
         l_gameDisplay.displayGame();
-
-
-=======
-            l_gameLogic.shipControl(event, m_window, m_shipTexture, m_shipSprite);
-            m_shipSprite.setPosition(l_gameLogic.shipPosition);
-        }
-
-        l_gameDisplay.displayGame(m_window, m_spaceSprite, m_shipSprite);
->>>>>>> 42ad3b5a7f3477d3e7179ce446a59c3c2d8df176
     }
 }
 
